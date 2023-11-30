8ae147307e12eea8140fa6c9da9272d53671b041
Anas Syed <123989080+anasyd@users.noreply.github.com>
2023-11-30 23:37:04 +0000

    Add Arrow
    added arrow that stays in front of the user and rotates with the camera

--- CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
+++ CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
@@ -1,42 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class cArrowManager : MonoBehaviour
-{ 
-    /* Singleton */
-    public static cArrowManager mInstance;                                              // Singleton instance, used to reference this class globally.
+{
+    [SerializeField]
+    private GameObject mArrowPrefab;
+    private GameObject mArrow;
 
     [SerializeField]
-    private GameObject mArrowPrefab;                                                    // Stores the arrow prefab to be instantiated later
+    private GameObject mCam;
 
-    private GameObject mArrow;                                                          // Stores the Arrow object not the prefab
-
-    [SerializeField]
-    private GameObject mCam;                                                            // Stores the camera object can't get the camera without it son dont remove it
-
-    /*------ Constants ---------*/
-    private const float kDistanceInfrontOfUser = 7.12f;                                 // How much distance in front of the user to place the arrow;
-
-    private const float kArrowYPosition = -1.04f;                                       // position the arrow a bit to the ground
-                   
-    /*------ Variables ---------*/
-    private int mCurrentTargetNodeIndex = cUser_Manager.kNullTargetNodeIndex;
-
-    private void Awake()
-    {
-        // Setup the singleton instance.
-        if (mInstance == null)
-        {
-            mInstance = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
+    private const float kDistanceInfrontOfUser = 7.12f;
+    private const float kArrowYPosition = -1.04f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,22 +24,18 @@ public class cArrowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mCurrentTargetNodeIndex = cUser_Manager.mInstance.GetTargetNode();                     // Gets the current target nodes index /If the target node is not set it returns -1 
-        if (mCurrentTargetNodeIndex == cUser_Manager.kNullTargetNodeIndex)
+        if (mArrow != null && mArrow.activeSelf)
         {
-            return;                                                                     // Doesn't run the updating code 
+            RotateArrow(10.0f*Time.deltaTime);
         }
-        
-        // Checks if the arrow is instantiated
-        if (mArrow == null)                                                        // Instantiate arrow if it isn't
+        else if (mArrow == null)
         {
-            mArrow = Instantiate(mArrowPrefab);                 
+            mArrow = Instantiate(mArrowPrefab);
         }
     }
 
     void FixedUpdate()
     {
-        // Keeps the arrow in front of the camera
         if (mArrow != null && mArrow.activeSelf)
         {
             mArrow.transform.position = mCam.transform.rotation * new Vector3(mCam.transform.position.x,
@@ -71,9 +44,9 @@ public class cArrowManager : MonoBehaviour
         }
     }
 
-    // Sets the arrow to a rotated position
     public void RotateArrow(float rotationAngle)
     {
-        mArrow.transform.rotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
+        mArrow.transform.Rotate(new Vector3(0.0f, 0.0f, rotationAngle));
+        //arrow.transform.SetPositionAndRotation()
     }
 }
