9a471f029edfdae3f3ef36cf742195d824d3eb06
Anas Syed <123989080+anasyd@users.noreply.github.com>
2023-11-30 23:50:44 +0000

    add comments

--- CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
+++ CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
@@ -7,12 +7,15 @@ public class cArrowManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject mArrowPrefab;
+
+    // Stores the Arrow object not the prefab
     private GameObject mArrow;
 
     [SerializeField]
     private GameObject mCam;
 
     private const float kDistanceInfrontOfUser = 7.12f;
+    // position the arrow a bit to the ground
     private const float kArrowYPosition = -1.04f;
 
     // Start is called before the first frame update
@@ -24,18 +27,21 @@ public class cArrowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks if the arrow is instantiated
         if (mArrow != null && mArrow.activeSelf)
         {
             RotateArrow(10.0f*Time.deltaTime);
         }
         else if (mArrow == null)
         {
+
             mArrow = Instantiate(mArrowPrefab);
         }
     }
 
     void FixedUpdate()
     {
+        // Keeps the arrow in front of the camera
         if (mArrow != null && mArrow.activeSelf)
         {
             mArrow.transform.position = mCam.transform.rotation * new Vector3(mCam.transform.position.x,
@@ -44,9 +50,9 @@ public class cArrowManager : MonoBehaviour
         }
     }
 
+    // Sets the arrow to a rotated position
     public void RotateArrow(float rotationAngle)
     {
         mArrow.transform.Rotate(new Vector3(0.0f, 0.0f, rotationAngle));
-        //arrow.transform.SetPositionAndRotation()
     }
 }
