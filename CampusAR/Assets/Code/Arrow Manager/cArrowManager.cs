ec21db4517098a7aa239325a195017fe371d4f11
Anas Syed <123989080+anasyd@users.noreply.github.com>
2023-12-01 11:56:16 +0000

    Add the check
    Displays the arrow only if the target node is selected

--- CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
+++ CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class cArrowManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mArrowPrefab;
+    private GameObject mArrowPrefab;                                                    // Stores the arrow prefab to be instantiated later
 
-    // Stores the Arrow object not the prefab
-    private GameObject mArrow;
+    private GameObject mArrow;                                                          // Stores the Arrow object not the prefab
 
     [SerializeField]
-    private GameObject mCam;
+    private GameObject mCam;                                                            // Stores the camera object can't get the camera without it son dont remove it
 
-    private const float kDistanceInfrontOfUser = 7.12f;
-    // position the arrow a bit to the ground
-    private const float kArrowYPosition = -1.04f;
+    /*------ Constants ---------*/
+    private const float kDistanceInfrontOfUser = 7.12f;                                 // How much distance in front of the user to place the arrow;
+
+    private const float kArrowYPosition = -1.04f;                                       // position the arrow a bit to the ground
+                   
+    /*------ Variables ---------*/
+    private int mCurrentTargetNodeIndex = cUser_Manager.kNullTargetNodeIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +31,21 @@ public class cArrowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mCurrentTargetNodeIndex = cUser_Manager.mInstance.GetTargetNode();                     // Gets the current target nodes index /If the target node is not set it returns -1 
+        if (mCurrentTargetNodeIndex == cUser_Manager.kNullTargetNodeIndex)
+        {
+            return;                                                                     // Doesn't run the updating code 
+        }
+        
         // Checks if the arrow is instantiated
         if (mArrow != null && mArrow.activeSelf)
         {
             RotateArrow(10.0f*Time.deltaTime);
         }
-        else if (mArrow == null)
+        else if (mArrow == null)                                                        // Instantiate arrow if it isn't
         {
 
-            mArrow = Instantiate(mArrowPrefab);
+            mArrow = Instantiate(mArrowPrefab);                 
         }
     }
 
