9e5e1320b6a8a066e3cd039062f8db03e52ac354
Iain Simpson <147500777+IainSimpsonCode@users.noreply.github.com>
2023-12-01 11:14:57 +0000

    Added mInstance to arrow manager to turn it into a singleton and keep it consistent with the other manager classes

--- CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
+++ CampusAR/Assets/Code/Arrow Manager/cArrowManager.cs
@@ -5,22 +5,34 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class cArrowManager : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject mArrowPrefab;                                                    // Stores the arrow prefab to be instantiated later
-
-    private GameObject mArrow;                                                          // Stores the Arrow object not the prefab
+{ 
+    /* Singleton */
+    public static cArrowManager mInstance;                                              // Singleton instance, used to reference this class globally.
 
     [SerializeField]
-    private GameObject mCam;                                                            // Stores the camera object can't get the camera without it son dont remove it
+    private GameObject mArrowPrefab;
 
-    /*------ Constants ---------*/
-    private const float kDistanceInfrontOfUser = 7.12f;                                 // How much distance in front of the user to place the arrow;
+    // Stores the Arrow object not the prefab
+    private GameObject mArrow;
 
-    private const float kArrowYPosition = -1.04f;                                       // position the arrow a bit to the ground
-                   
-    /*------ Variables ---------*/
-    private int mCurrentTargetNodeIndex = cUser_Manager.kNullTargetNodeIndex;
+    [SerializeField]
+    private GameObject mCam;
+
+    private const float kDistanceInfrontOfUser = 7.12f;
+    // position the arrow a bit to the ground
+    private const float kArrowYPosition = -1.04f;
+    private void Awake()
+    {
+        // Setup the singleton instance.
+        if (mInstance == null)
+        {
+            mInstance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -31,21 +43,15 @@ public class cArrowManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mCurrentTargetNodeIndex = cUser_Manager.mInstance.GetTargetNode();                     // Gets the current target nodes index /If the target node is not set it returns -1 
-        if (mCurrentTargetNodeIndex == cUser_Manager.kNullTargetNodeIndex)
-        {
-            return;                                                                     // Doesn't run the updating code 
-        }
-        
         // Checks if the arrow is instantiated
         if (mArrow != null && mArrow.activeSelf)
         {
             RotateArrow(10.0f*Time.deltaTime);
         }
-        else if (mArrow == null)                                                        // Instantiate arrow if it isn't
+        else if (mArrow == null)
         {
 
-            mArrow = Instantiate(mArrowPrefab);                 
+            mArrow = Instantiate(mArrowPrefab);
         }
     }
 
