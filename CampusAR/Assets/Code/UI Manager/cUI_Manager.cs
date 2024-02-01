a546079d4cb7225cf97c9e6ec99cb87ea7538ec1
Lewis Davidson <lewisdavidson8@gmail.com>
2024-02-01 18:51:53 +0000

    Bottom Drawer lerp animation code

--- CampusAR/Assets/Code/UI Manager/cUI_Manager.cs
+++ CampusAR/Assets/Code/UI Manager/cUI_Manager.cs
@@ -10,6 +10,8 @@ public class cUI_Manager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI rBuildingNameField;
     private cNode currentBuildingNode; // Reference to the current cNode_Building instance
 
+    [SerializeField] private Canvas bottomDrawerCanvas; // Reference to bottom drawer component
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +46,7 @@ public class cUI_Manager : MonoBehaviour
     public void HandleSelectBuildingButton()
     {
         Debug.Log("You have clicked the SelectBuilding button!");
+        StartCoroutine(AnimateCanvasPosition());
     }
     public void HandleSelectTourButton()
     {
@@ -58,5 +61,49 @@ public class cUI_Manager : MonoBehaviour
     {
         Debug.Log("You have clicked the Settings button!");
         SceneManager.LoadScene("SettingsScene");
+    }
+
+    IEnumerator AnimateCanvasPosition()
+    {
+        Vector3 startPosition = bottomDrawerCanvas.transform.position;
+        Vector3 targetPosition = new Vector3(startPosition.x, 100, startPosition.z);
+        float duration = 1.0f;
+        float startTime = Time.time;
+
+        while (Time.time - startTime < duration)
+        {
+            float timeElapsed = Time.time - startTime;
+            float t = Mathf.Clamp01(timeElapsed / duration);
+            float curveValue = CustomBezier(t, 0.32f, 0.72f, 0.0f, 1.0f);
+            bottomDrawerCanvas.transform.position = Vector3.Lerp(startPosition, targetPosition, curveValue);
+            yield return null;
+        }
+    }
+
+    IEnumerator AnimateCanvasPositionBack()
+    {
+        float duration = 1.0f;
+        float startTime = Time.time;
+        Vector3 targetPosition = new Vector3(540, -1074, 0);
+
+        while (Time.time - startTime < duration)
+        {
+            float timeElapsed = Time.time - startTime;
+            float t = Mathf.Clamp01(timeElapsed / duration);
+            float curveValue = CustomBezier(t, 0.08f, 0.82f, 0.17f, 1.0f);
+            bottomDrawerCanvas.transform.position = Vector3.Lerp(bottomDrawerCanvas.transform.position, targetPosition, curveValue * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    float CustomBezier(float t, float a, float b, float c, float d)
+    {
+        float s = 1.0f - t;
+        float ab = Mathf.Lerp(a, b, t);
+        float bc = Mathf.Lerp(b, c, t);
+        float cd = Mathf.Lerp(c, d, t);
+        float abc = Mathf.Lerp(ab, bc, t);
+        float bcd = Mathf.Lerp(bc, cd, t);
+        return Mathf.Lerp(abc, bcd, t);
     }
 }
