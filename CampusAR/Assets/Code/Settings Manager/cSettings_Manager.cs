8a271c586afec7749a4906fff5f157e316358d13
Lewis Davidson <lewisdavidson8@gmail.com>
2024-02-02 14:43:13 +0000

    Settings dropdown logs value as string
    TODO save string into PlayerPrefs to persist between scenes
    UI scene has bottom drawer panel
    TODO prefab for list of buildings

--- CampusAR/Assets/Code/Settings Manager/cSettings_Manager.cs
+++ CampusAR/Assets/Code/Settings Manager/cSettings_Manager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class cSettings_Manager : MonoBehaviour
 {
+
+    [SerializeField] private TMPro.TMP_Dropdown tmpDropdown;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +26,10 @@ public class cSettings_Manager : MonoBehaviour
     public void HandleGoBackButton()
     {
         SceneManager.LoadScene("UserInterface");
+    }
+
+    public void SaveMetricsPreference()
+    {
+        Debug.Log(tmpDropdown.options[tmpDropdown.value].text);
     }
 }
